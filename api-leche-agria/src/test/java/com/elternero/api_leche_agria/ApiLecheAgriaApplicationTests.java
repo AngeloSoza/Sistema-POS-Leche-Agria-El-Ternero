68f62da06244d8924637b83d45b0c1ae5b7ae4a7
AngeloSoza <angelosoza@uamv.edu.ni>
2026-06-04 17:38:37 -0600

    Finalizo integración uniendo backend, frontend y base de datos

---
+++ api-leche-agria/src/test/java/com/elternero/api_leche_agria/ApiLecheAgriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elternero.api_leche_agria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLecheAgriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
