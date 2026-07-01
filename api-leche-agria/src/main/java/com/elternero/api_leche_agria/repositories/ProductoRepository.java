6030d1f05fd555cad0a88162e4b8b19e0e22c648
Angelo_Soza <164054849+AngeloSoza@users.noreply.github.com>
2026-07-01 12:51:36 -0600

    Actualización final de ProductoRepository

--- api-leche-agria/src/main/java/com/elternero/api_leche_agria/repositories/ProductoRepository.java
+++ api-leche-agria/src/main/java/com/elternero/api_leche_agria/repositories/ProductoRepository.java
@@ -4,4 +4,4 @@ import com.elternero.api_leche_agria.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
+} 
